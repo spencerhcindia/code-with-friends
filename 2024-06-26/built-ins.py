99b2f9e796ad5de5ef8ebbeab5ad591f0f957092
spencerhcindia <107213392+spencerhcindia@users.noreply.github.com>
2024-07-17 21:38:05 -0400

    learnig collections etc

--- 2024-06-26/built-ins.py
+++ 2024-06-26/built-ins.py
@@ -1,6 +1,8 @@
 # A function that uses itertools.count and counts from a start arg by 6 until it reaches a target and then returns the number it reaches
 
 from itertools import count, cycle, chain
+from random import choices, sample
+from collections import defaultdict, Counter, deque
 
 def my_count(start: int, target: int) -> list:
     for i in count(start, 6):
@@ -46,4 +48,82 @@ def combine(*lists: list) -> list:
     for i in chain(*lists):
         my_list.append(i)
     return list(set(my_list))
-print(combine([1,2,3], ["a","b"], ["butt"], ['p', 'i', 'n', 'u', 's'], [1,2]))
+# print(combine([1,2,3], ["a","b"], ["butt"], ['p', 'i', 'n', 'u', 's'], [1,2]))
+
+# A function for an int k returns a string of length k that is randomly generated from letters a-z
+# and numbers 0-5 using random.choices
+
+def rand_string(k: int) -> str:
+    alphabet = 'abcdefghijklmnopqrstuvwxyz012345'
+    return "".join(choices(alphabet, k=k))
+
+# print(rand_string(20))
+
+# A function that, given a string, uses random.sample to scramble the str and hand it back to you
+# params = return type str
+
+def scramble(string: str) -> str:
+    result = "".join(sample(string, len(string)))
+    return result
+
+# print(scramble("Mom and Dad are fighting again and it's scaring me more than usual."))
+
+def number_sort(k: int) -> dict:
+    shape = defaultdict(list)
+    for i in range(k+1):
+        if i % 2 == 0:
+            shape["even"].append(i)
+        if i % 2 != 0:
+            shape["odd"].append(i)
+        if i % 3 == 0:
+            shape["divisible_3"].append(i)
+        if i % 5 == 0:
+            shape["divisible_5"].append(i)
+    return dict(shape)
+
+# print(number_sort(30))
+
+# A function that given a string utilizes collections.counter to return the letter in the string
+# with the highest occurrence
+#
+# if there's a tie it returns none
+#
+# the following strings:
+# hyderabad
+# tambourine
+# scintillating
+# ramparts
+# championship
+
+def letter_finder(string: str) -> str | None:
+
+    letters = dict(Counter(string)) # this is a dict with key letter and value num of occurrences
+
+    maximum = max(letters.values()) # this is the highest number of all the values in the above dict
+
+    if dict(Counter(letters.values()))[maximum] > 1:
+        return None
+    else:
+        for element, value in letters.items():
+            if value == maximum:
+                return element
+
+
+
+# print(letter_finder("bamboo"))
+
+# A function that given a string, adds each letter of the string to the deque
+# pops them off in the reverse order and thus prints the string in reverse order.
+
+def reverse_string(string: str) -> str:
+    my_deque = deque()
+    my_stash = ""
+    for i in string:
+        my_deque.append(i)
+        print(my_deque)
+    for i in range(len(string)):
+        my_stash += my_deque.pop()
+        print(my_deque)
+    return my_stash
+
+print(reverse_string("mokney kog"))
